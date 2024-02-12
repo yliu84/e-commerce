d3b1dd7b9f80f7ff130ffb49540b880fbaf011fe
yliu84 <yliu84@studentmail.nait.ca>
2024-02-11 20:43:49 -0700

    updated logo

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -42,7 +42,7 @@ const FooterComponent = ({ footer }: { footer: Footer }) => {
         <Gutter>
           <div className={classes.wrap}>
             <Link href="/">
-              <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
+              <Image src="/logo.png" alt="logo" width={170} height={50} />
             </Link>
 
             <p>{footer.copyright}</p>
